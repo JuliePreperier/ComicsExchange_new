96eed6329fa7cc39d2f3be5766ce7db2aac8eace
Sandy <sandy.millius@students.hevs.ch>
2017-04-28 15:28:14 +0200

    Commit Sandy 28.04 à 15h28

--- app/src/main/java/com/example/comicsexchange_new/ExchangeFragment.java
+++ app/src/main/java/com/example/comicsexchange_new/ExchangeFragment.java
@@ -21,7 +21,7 @@ public class ExchangeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        getActivity().setTitle("I exchange");
+        getActivity().setTitle("I exchangeTest");
         return inflater.inflate(R.layout.fragment_exemple, container, false);
     }
 
