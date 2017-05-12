804f90fc020a7127ecf99e8279de37d0839694c4
juliePreperier <julie.preperier@students.hevs.ch>
2017-05-12 15:07:40 +0200

    commit first Cloud branch

---
+++ backend/src/main/java/entities/Authors.java
@@ -0,0 +1,41 @@
+package entities;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+/**
+ * Created by Julie on 12.05.2017.
+ */
+@Entity
+public class Authors {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Authors(){
+
+    }
+
+
+
+}
---
+++ backend/src/main/java/entities/User.java
@@ -0,0 +1,60 @@
+package entities;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+import javax.jws.soap.SOAPBinding;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+/**
+ * Created by Julie on 12.05.2017.
+ */
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User(){
+
+    }
+
+
+
+}
